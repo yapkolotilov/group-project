383cf7b77bd33338baff1a6b85f971d5df9e45e8
Yaroslav <yaroslavcoworker@gmail.com>
2021-01-30 18:33:18 +0300

    Initial Commit

---
+++ src/main/kotlin/me/kolotilov/groupproject/GroupProjectApplication.kt
@@ -0,0 +1,11 @@
+package me.kolotilov.groupproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroupProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<GroupProjectApplication>(*args)
+}
